22815159f2de6fa37b8f16f3624eddd32d76f7c9
byntmr <56450129+byntmr@users.noreply.github.com>
2019-11-08 13:44:25 +0400

    Add files via upload

--- Homework/src/hw1/numbers.java
+++ Homework/src/hw1/numbers.java
@@ -1,4 +1,45 @@
 package hw1;
 
-public class numbers {qwewe
+import java.util.Random;
+import java.util.Scanner;
+
+public class numbers {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int random1 = random.nextInt(100) + 1;
+        int guessesList[] = new int[99999];
+        int guess;
+        int count = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.next();
+        System.out.println("Let the game begin!");
+
+        guess = sc.nextInt();
+        guessesList[0] = guess;
+        count++;
+        while (true) {
+
+            if (guess > random1) {
+                System.out.println("Your number is too big. Please, try again.");
+                guess = sc.nextInt();
+                guessesList[count] = guess;
+                count++;
+            } else if (guess < random1) {
+                System.out.println("Your number is too small. Please, try again.");
+                guess = sc.nextInt();
+                guessesList[count] = guess;
+                count++;
+            } else if (guess == random1) break;
+
+
+        }
+        System.out.println("Congratulations," + name + "!");
+        System.out.println("Your guesses: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(guessesList[i] + " ");
+
+        }
+    }
 }
+
